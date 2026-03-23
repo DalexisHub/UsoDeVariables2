b9502e19e8817902bab8983c1fccc9e9d9d7f44e
DalexisHub <alexisdiego478@gmail.com>
2026-03-23 09:04:24 -0500

    primer commit caso 01

--- UsoDeVariables.playground/Contents.swift
+++ UsoDeVariables.playground/Contents.swift
@@ -1,3 +1,19 @@
-import Cocoa
+let INVERSION_INICIAL = 200000.0
+let TASA_INTERES = 0.04
 
-var greeting = "Hello, playground"
+let INTERESES_GENERADOS = INVERSION_INICIAL * TASA_INTERES
+var monto_final = INVERSION_INICIAL
+
+if INTERESES_GENERADOS > 7000 {
+    monto_final += INTERESES_GENERADOS
+}
+
+print("Intereses generados: $\(INTERESES_GENERADOS)")
+
+if INTERESES_GENERADOS > 7000 {
+    print("Los intereses superan $7000, por lo tanto se reinvierten.")
+} else {
+    print("Los intereses no superan $7000, por lo tanto no se reinvierten.")
+}
+
+print("Capital final en la cuenta: $\(monto_final)")
